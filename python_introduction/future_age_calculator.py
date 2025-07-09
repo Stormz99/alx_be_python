ff392988ff567f888b3b1fb410fe4de4df1f7258
Abiodun <ijiolastormz@gmail.com>
2025-07-10 00:11:07 +0100

    Fixed input prompt and logic for Age Calculator task

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,8 +1,5 @@
-#define the variables
-acutal_age = int(input(f"How old are you? :"))
+# future_age_calculator.py
 
-# Calculate the future age in 2050
-future_age = acutal_age + 27
-
-# Print the result
-print(f"In 2050, you will be {future_age} years old.")
+current_age = int(input("How old are you? "))
+age_in_2050 = current_age + 27
+print(f"In 2050, you will be {age_in_2050} years old.")
