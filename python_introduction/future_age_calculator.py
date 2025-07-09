f91b85946b779d232b6f3ab94c4279daecb772c2
Abiodun <ijiolastormz@gmail.com>
2025-07-10 00:06:39 +0100

    Intial commit of Age Calculator Task

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+#define the variables
+acutal_age = int(input(f"Enter your current age:"))
+
+# Calculate the future age in 2050
+future_age = acutal_age + 27
+
+# Print the result
+print(f"In 2050, you will be {future_age} years old.")
