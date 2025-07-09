60f739fba917f7436bd18c350791d6934e938a04
Abiodun <ijiolastormz@gmail.com>
2025-07-09 23:55:16 +0100

    Intial commit of the area of the rectangle task

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,10 @@
+#Define the variables
+length = 10  # Length of the rectangle in meters
+width = 5  # Width of the rectangle in meters
+
+# Calculate the area of the rectangle
+#area = length * width
+area = length * width
+
+# print the result
+print(f"The area of the rectangle is: {area}")
