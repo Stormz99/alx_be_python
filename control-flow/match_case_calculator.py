31bda8a2ba12cb663f1ccd85ba225a9c73072833
Abiodun <ijiolastormz@gmail.com>
2025-07-18 23:21:49 +0100

    fixed checker requirements

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,18 +1,18 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-operation = input("Enter the operation (+, -, *, /): ").strip()
+operation = input("Enter the operation (+, -, *, /): ")
 
 match operation:
     case "+":
-        print(f"The result of {num1} + {num2} is {num1 + num2}.")
+        print(f"The result of {num1} + {num2}")
     case "-":
-            print(f"The result of {num1} - {num2} is {num1 - num2}.")
+            print(f"The result of {num1} - {num2}")
     case "*":
-        print(f"The result of {num1} * {num2} is {num1 * num2}.")
+        print(f"The result of {num1} * {num2}")
     case "/":
         if num2 == 0:
             print(f"Cannot divide by zero.")
         else:
-            print(f"The result of {num1} / {num2} is {num1 / num2}.")
+            print(f"The result of {num1} / {num2}")
     case _:
-        print(f"Invalid operation '{operation}'. Please use +, -, *, or /.")
+        print(f"Invalid operation '{operation}")
