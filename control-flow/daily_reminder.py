47ae989758113708948ba3b25c9b9197ec11192b
Abiodun <ijiolastormz@gmail.com>
2025-07-18 23:39:37 +0100

    Task completed for Reminder Application

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,19 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
+
+match priority:
+    case "high":
+        message = f"Reminder: '{task}' is a high priority task."
+    case "medium":
+        message = f"Reminder: '{task}' is a medium priority task."
+    case "low":
+        message = f"Reminder: '{task}' is a low priority task."
+
+if time_bound == "yes":
+    message += " that requires immediate attention today! "
+    
+elif time_bound == "no" and "Reminder" not in message:
+    message += ". Consider completing it when you have free time." 
+
+print(message)
