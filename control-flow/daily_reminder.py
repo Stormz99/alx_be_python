3907339d27ee98415404e532f11c226c1ae6f8a7
Abiodun <ijiolastormz@gmail.com>
2025-07-18 23:41:52 +0100

    Fixed checker failed for Note addition

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,16 +4,15 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
     case "high":
-        message = f"Reminder: '{task}' is a high priority task."
+        message = f"Reminder: '{task}' is a high priority task"
     case "medium":
-        message = f"Reminder: '{task}' is a medium priority task."
+        message = f"Note: '{task}' is a medium priority task"
     case "low":
-        message = f"Reminder: '{task}' is a low priority task."
+        message = f"Note: '{task}' is a low priority task"
+    case _:
+        message = f"Note: '{task}' has an unknown priority"
 
-if time_bound == "yes":
-    message += " that requires immediate attention today! "
-    
-elif time_bound == "no" and "Reminder" not in message:
-    message += ". Consider completing it when you have free time." 
+if time_bound == "yes" and priority == "high":
+    message += " that requires immediate attention today!"
 
 print(message)
