50ab78ea4b23d507b0d2c67cf1405399d027abdc
Abiodun <ijiolastormz@gmail.com>
2025-07-18 23:15:05 +0100

    fixed task0

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
-weather = input("Enter the current weather (sunny, rainy, snowy, cloudy): ").strip().lower()
+weather = input("Enter the current weather (sunny/rainy/cold): ").strip().lower()
 if weather == "sunny":
-    print(f"Wear a t-shirt and sunglasses.")
+    print("Wear a t-shirt and sunglasses.")
 elif weather== "rainy":
-    print(f"Don't forget your umberella and raincoat.")
+    print("Don't forget your umbrella and raincoat.")
 elif weather == "cold":
-    print(f"Make sire to wear a warm coat and scarf.")
+    print("Make sure to wear a warm coat and scarf.")
 else:
-    print(f"Sorry, I don't have recommendations for this weather.")
+    print("Sorry, I don't have recommendations for this weather.")
